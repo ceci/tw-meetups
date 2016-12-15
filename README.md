6198e3f381b6aad49c09d658f4f92eb436ffe249
Ceci Fernandes <cfernand@thoughtworks.com>
2016-12-15 15:47:58 -0200

    Removing Node from project, in order to provide it from github.io

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # tw-meetups
 
-This will be the meetup schedule for local events on TW Sao Paulo office.
+This will be the meet-up schedule for local events on TW Sao Paulo office.
 
 ---
-
-It is also a project for practicing Node and front-end dev for people on the beach.
+It is also a project for practicing front-end dev for people on the beach.
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,3 @@
-var express = require('express');
-var router = express.Router();
-
 let upcoming = [
     {name: 'Chatbots', date: new Date(2016, 11, 6, 19, 0), description: 'Vamos discutir como os chatbots estão mudando a forma de nos relacionarmos com nossos clientes', registerAt: 'https://www.sympla.com.br/chatbot-brasil--1-meetup__103900', targetAudience:'UX'},
     {name: 'Ladies that UX', date: new Date(2016, 11, 8, 19, 30), description: 'Meetup internacional focado em discussões de UX de e para profissionais mulheres', registerAt: '#', targetAudience: 'UX'},
@@ -8,9 +5,4 @@ let upcoming = [
     {name: 'Último evento do ano', date: new Date(2016, 11, 22, 20, 0), description: 'Esse evento reunirá pessoas de TI que estejam a fim de discutir como fazer de 2017 um ano onde usamos a tecnologia melhor', registerAt: '#', targetAudience: 'Pessoas de TI'}
 ];
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { meetups: upcoming});
-});
-
-module.exports = router;
+console.log('It ran!');
